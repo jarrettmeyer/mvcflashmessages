4cbfa3074d99c81f014f75a72c3c12a807db9321
Jarrett Meyer <jarrettmeyer@gmail.com>
2013-12-18 07:45:12 -0500

    initial commit of sample library

---
+++ source/Sample/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using MvcFlashMessages;
+
+namespace Sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            this.Flash("info", "You were redirected.");
+            return RedirectToAction("Index", "Products");
+        }
+    }
+}
---
+++ source/Sample/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+
+namespace Sample.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ProductRepository repository;
+
+        public ProductsController()
+        {
+            repository = new ProductRepository();
+        }
+
+        public ActionResult Index()
+        {
+            var products = repository.FindAll();
+            return View("Index", products);
+        }
+
+    }
+}
